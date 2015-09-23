fa543bc528e35cb48056a16847c31c7269702175
mohan82 <megeek69>
2015-09-23 11:46:17 +1000

    Install vim from scratch implementation complete

--- install-vim.sh
+++ install-vim.sh
@@ -1,7 +1,15 @@
+##Assumes vim/gvim is installed in your PC
 #Install  vundle
 rm -rf ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 #Install monokai scheme
 rm -rf ~/.vim/colors
- ~/.vim/colors/monokai.vim
+mkdir -p ~/.vim/colors
+curl  https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim  -o ~/.vim/colors/monokai.vim
+
+Download vimrc 
+curl  https://raw.githubusercontent.com/mohan82/dot-files/master/.vimrc  -o ~/.vimrc
+
+#Install Vim Plugins
+vim +PluginInstall
